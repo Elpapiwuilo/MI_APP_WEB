dae121ae0eef19e608d793352eff35a3d470e4f0
Sebastian <sebasipi501@gmail.com>
2019-05-14 14:41:32 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MI_APP_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
